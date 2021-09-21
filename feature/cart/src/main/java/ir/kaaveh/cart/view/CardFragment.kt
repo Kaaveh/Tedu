bae6e13c1d7ec28f65101cd327989368c51b9129
kaaveh <kaaveh@pm.me>
2021-09-21 14:29:18 +0430

    Add CardFragment

--- feature/cart/src/main/java/ir/kaaveh/cart/view/CardFragment.kt
+++ feature/cart/src/main/java/ir/kaaveh/cart/view/CardFragment.kt
@@ -1,12 +1,17 @@
 package ir.kaaveh.cart.view
 
 import android.content.Context
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
 import com.phelat.tedu.androiddagger.inject
 import com.phelat.tedu.lifecycle.ViewModelFactory
 import ir.kaaveh.cart.R
+import ir.kaaveh.cart.databinding.FragmentCardBinding
 import ir.kaaveh.cart.di.component.CardComponent
+import ir.kaaveh.cart.utils.viewLifecycleLazy
 import ir.kaaveh.cart.viewmodel.CardViewModel
 import javax.inject.Inject
 
@@ -15,9 +20,17 @@ class CardFragment : Fragment(R.layout.fragment_card) {
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
     private val viewModel: CardViewModel by viewModels { viewModelFactory }
+    private val binding by viewLifecycleLazy { FragmentCardBinding.bind(requireView()) }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         inject<CardComponent>()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.shopCard.observe(viewLifecycleOwner) { shopCard ->
+            binding.txtShopCard.text = shopCard.toString()
+        }
     }
 }
